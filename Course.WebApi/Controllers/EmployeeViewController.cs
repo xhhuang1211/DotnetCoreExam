36334b4d0649766b74548ba59512ec94ad9f326c
tomlee <tomlee@nine-yi.com>
2019-12-13 13:29:47 +0800

    exam

---
+++ Course.WebApi/Controllers/EmployeeViewController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Course.WebApi.Repositories;
+using Course.WebApi.Interfaces;
+using System.Linq;
+using Course.WebApi.Models;
+
+namespace Course.WebApi.Controllers
+{
+
+    [Route("[controller]/[Action]")]
+    public class EmployeeViewController : Controller
+    {
+        private readonly IEmployeeRepo _employeeRepo;
+        public EmployeeViewController(IEmployeeRepo employeeRepo)
+        {
+            _employeeRepo = employeeRepo;
+        }
+        public IActionResult List()
+        {
+            return View(_employeeRepo.Read());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromForm]Employee employee)
+        {
+            _employeeRepo.Create(employee);
+            return RedirectToAction("List", "EmployeeView");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var result = _employeeRepo.Read().FirstOrDefault(x => x.Id == id);
+            if (result == null)
+            {
+                return RedirectToAction("List", "EmployeeView");
+            }
+            return View(result);
+        }
+
+        [HttpPost]
+        public IActionResult Edit([FromForm]Employee employee, [FromQuery]int id)
+        {
+            _employeeRepo.Update(employee, id);
+            return RedirectToAction("List", "EmployeeView");
+        }
+
+        public IActionResult Delete([FromQuery]int id)
+        {
+            _employeeRepo.Delete(id);
+            return RedirectToAction("List", "EmployeeView");
+        }
+    }
+}
---
+++ Course.WebApi/Repositories/EmployeeRepo.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Course.WebApi.Interfaces;
+using Course.WebApi.Models;
+using Course.WebApi.Repositories;
+
+namespace Course.WebApi.Services
+{
+    public class EmployeeRepo :IEmployeeRepo
+    {
+        private readonly MyCompanyDbContext _context;
+        public EmployeeRepo(MyCompanyDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Employee> Create(Employee employee)
+        {
+            _context.Employees.Add(employee);
+            _context.SaveChanges();
+            return _context.Employees.ToArray();
+        }
+
+        public IEnumerable<Employee> Read()
+        {
+            return _context.Employees.ToArray();
+        }
+
+        public IEnumerable<Employee> Update(Employee employee, int id)
+        {
+            var item = _context.Employees.SingleOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                // item.Summary = employee.Summary;
+                // item.TemperatureC = employee.TemperatureC;
+                // item.Date = employee.Date;
+                _context.SaveChanges();
+            }
+            return _context.Employees.ToArray();
+        }
+        
+
+        public IEnumerable<Employee> Delete(int id)
+        {
+            var item = _context.Employees.SingleOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                _context.Employees.Remove(item);
+                _context.SaveChanges();
+            }
+            return _context.Employees.ToArray();
+        }
+    }
+}
