6467d30309c63bb1b45b5f34fdedf1707ec2dde9
AlbertKao <albertkao@nine-yi.com>
2019-12-16 11:12:59 +0800

    Create Employee model & db context

---
+++ Course.WebApi/Models/Employee.cs
@@ -0,0 +1,21 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Course.WebApi.Models
+{
+    [Table("Employee")] //自訂db table name 
+    public class Employee
+    {
+        [Key]//主键
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]//透過db自動產出的唯一識別值
+        [Column("id")] //自訂db table 欄位名稱 
+        public int Id { get; set; }
+
+        [Column("name")]
+        public string Name { get; set; }
+
+        [Column("salary")]
+        public string Salary { get; set; }
+    }
+}
---
+++ Course.WebApi/Repositories/EmployeeDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Course.WebApi.Models;
+
+namespace Course.WebApi.Repositories
+{
+    
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet 用來指定要使用的 table類別
+        public DbSet<Employee> WeatherForecasts { get; set; }
+    }
+}
