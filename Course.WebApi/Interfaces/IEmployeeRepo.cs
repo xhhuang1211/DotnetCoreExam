2b7c3762906c4eac1a3237bfd78ec6970adde354
samuelchen <samuelchen@nine-yi.com>
2019-12-16 10:50:06 +0800

    db can not run

---
+++ Course.WebApi/Interfaces/IEmployeeRepo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Course.WebApi.Models;
+
+namespace Course.WebApi.Interfaces
+{
+    public interface IEmployeeRepo
+    {
+        IEnumerable<Employee> Read();
+        IEnumerable<Employee> Update(Employee employee, int id);
+    }
+}
---
+++ Course.WebApi/Repositories/EmployeeRepo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Course.WebApi.Interfaces;
+using Course.WebApi.Models;
+using Course.WebApi.Repositories;
+
+namespace Course.WebApi.Services
+{
+    public class EmployeeRepo :IEmployeeRepo
+    {
+        private readonly MyCompanyDbContext _context;
+        public EmployeeRepo(MyCompanyDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Employee> Read()
+        {
+            return _context.Employees.ToArray();
+        }
+
+        public IEnumerable<Employee> Update(Employee employee, int id)
+        {
+            var item = _context.Employees.SingleOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                item.Name = employee.Name;
+                item.Salary = employee.Salary;
+                _context.SaveChanges();
+            }
+            return _context.Employees.ToArray();
+        }
+
+        public IEnumerable<Employee> Delete(int id)
+        {
+            var item = _context.Employees.SingleOrDefault(x => x.Id == id);
+            if (item != null)
+            {
+                _context.Employees.Remove(item);
+                _context.SaveChanges();
+            }
+            return _context.Employees.ToArray();
+        }
+    }
+}
